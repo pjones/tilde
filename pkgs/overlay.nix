7976858a567a12482d4cf7aa18101ac2d60e0216
Peter Jones <pjones@devalot.com>
2020-05-15 14:17:58 -0700

    nixfmt changes

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -1,6 +1,4 @@
-{ sources ? import ../nix/sources.nix
-, ghc ? "default"
-}:
+{ sources ? import ../nix/sources.nix, ghc ? "default" }:
 
 # Arguments to the overlay function:
 self: super:
@@ -15,8 +13,9 @@ let
   # Load a package and call its function with any arguments that it
   # requests from the `args' set above.
   load = package: args:
-    let fn = import package;
-        fargs = builtins.functionArgs fn;
+    let
+      fn = import package;
+      fargs = builtins.functionArgs fn;
     in fn (super.lib.filterAttrs (n: _: fargs ? ${n}) args);
 
   # My packages have this prefix:
@@ -27,10 +26,12 @@ let
     mapAttrs' (n: v: nameValuePair (removePrefix prefix n) (load v args))
     (filterAttrs (n: _: hasPrefix prefix n) sources);
 
-in
-{
+in {
   pjones = pjones // {
     # Edify can only build with GHC 8.8.3 right now:
-    edify = load sources."pjones/edify" { ghc = "883"; pkgs = super; };
+    edify = load sources."pjones/edify" {
+      ghc = "883";
+      pkgs = super;
+    };
   };
 }
