265c5f89b6d90d89ef11513f75b40712b8536375
Peter Jones <pjones@devalot.com>
2020-10-19 08:56:57 -0700

    Allow keyboards to be accessible in user space
    https://github.com/qmk/qmk_firmware/blob/master/docs/faq_build.md#linux-udev-rules

--- nixos/keyboard.nix
+++ nixos/keyboard.nix
@@ -21,16 +21,60 @@ let
     sudo -u ${config.tilde.username} ${package}/bin/keyboard.sh "$@"
   '';
 
+  deviceOpts = {
+    options = {
+      vendor = lib.mkOption {
+        type = lib.types.str;
+        example = "03EB";
+        description = "USB vendor ID provided by lsusb";
+      };
+
+      product = lib.mkOption {
+        type = lib.types.str;
+        example = "2FEF";
+        description = "USB product ID provided by lsusb";
+      };
+    };
+  };
+
 in
 {
   options.tilde.workstation.keyboard = {
     enable = lib.mkEnableOption "Custom keyboard scripts";
+
+    devices = lib.mkOption {
+      type = lib.types.listOf (lib.types.submodule deviceOpts);
+      description = "List of keyboard devices";
+      default = [
+        { vendor = "feed"; product = "3060"; } # Dactyl Manuform Mini
+        { vendor = "feed"; product = "0000"; } # Redox
+      ];
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    services.udev.extraRules = ''
-      ACTION=="add",    ATTRS{idVendor}=="feed", ATTRS{idProduct}=="3060", RUN+="${script} add"
-      ACTION=="remove", ENV{PRODUCT}=="feed/3060/1", RUN+="${script} rm"
-    '';
+    services.udev.extraRules =
+      lib.concatMapStringsSep "\n"
+        (kbd:
+          let
+            add = [
+              ''SUBSYSTEMS=="usb"''
+              ''ACTION=="add"''
+              ''ATTRS{idVendor}=="${kbd.vendor}"''
+              ''ATTRS{idProduct}=="${kbd.product}"''
+              ''TAG+="uaccess"''
+              ''RUN{builtin}+="uaccess"''
+              ''RUN+="${script} add"''
+            ];
+            remove = [
+              ''SUBSYSTEMS=="usb"''
+              ''ACTION=="remove"''
+              ''ATTRS{idVendor}=="${kbd.vendor}"''
+              ''ATTRS{idProduct}=="${kbd.product}"''
+              ''RUN+="${script} rm"''
+            ];
+          in
+          lib.concatStringsSep "," add + "\n" + lib.concatStringsSep "," remove)
+        cfg.devices;
   };
 }
