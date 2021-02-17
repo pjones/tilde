f1f4d4fcd2904d37db20e4c851107a5a0af626d3
Peter Jones <pjones@devalot.com>
2021-02-17 10:47:43 -0700

    Disable wifi when docking with my treadmill

--- devices/elphaba.nix
+++ devices/elphaba.nix
@@ -32,6 +32,10 @@
             name = "Treadmill Docking Station";
             outputs_connected = [ treadmill ];
             configure_single = external;
+            execute_after = [
+              # Use the dock's Ethernet connection:
+              "${pkgs.networkmanager}/bin/nmcli radio wifi off"
+            ];
           }
           {
             name = "Dual Monitors";
@@ -42,6 +46,10 @@
           {
             name = "Internal Screen Only";
             configure_single = internal;
+            execute_after = [
+              # Ensure WiFi is restored:
+              "${pkgs.networkmanager}/bin/nmcli radio wifi on"
+            ];
           }
         ];
     };
