571e23cf58e30c13e6099ab386f6d1dacdf415d9
Peter Jones <pjones@devalot.com>
2022-05-16 14:46:53 -0700

    plasma: Capture more settings

--- home/programs/plasma.nix
+++ home/programs/plasma.nix
@@ -21,7 +21,11 @@ let
           kWriteConfig "kglobalshortcutsrc" group
             (lib.mapAttrs' bindingToNameValuePair attrs)
         )
-        cfg.bindings));
+        (lib.recursiveUpdate cfg.bindings hotkeysToBindings))
+    + lib.concatStrings
+      (lib.mapAttrsToList
+        (kWriteConfig "khotkeysrc")
+        hotkeysToKConfig));
 
   toKdeValue = v:
     if v == null then
@@ -44,6 +48,77 @@ let
     ];
   };
 
+  # Extract hotkey shortcuts for the kglobalshortcutsrc file:
+  hotkeysToBindings = {
+    khotkeys = builtins.listToAttrs (lib.flatten
+      (builtins.map
+        (group:
+          (builtins.map
+            (hotkey: {
+              name = "{${hotkey.uuid}}";
+              value = {
+                name = "{${hotkey.uuid}}";
+                keys = [ hotkey.key ];
+                display = hotkey.name;
+                defaults = [ ];
+              };
+            })
+            (builtins.attrValues group)))
+        (builtins.attrValues cfg.hotkeys)));
+  };
+
+  hotkeysToKConfig =
+    let
+      groups = lib.foldr lib.recursiveUpdate { } (lib.imap1
+        (i: group: lib.recursiveUpdate
+          {
+            "Data_${toString i}" = {
+              Comment = group;
+              DataCount = builtins.length (builtins.attrNames cfg.hotkeys.${group});
+              Enabled = true;
+              Name = group;
+              SystemGroup = 0;
+              Type = "ACTION_DATA_GROUP";
+            };
+            "Data_${toString i}Conditions".ConditionsCount = 0;
+          }
+          (items i group))
+        (builtins.attrNames cfg.hotkeys));
+
+      items = i: group: lib.foldr lib.recursiveUpdate { } (lib.imap1
+        (j: item: {
+          "Data_${toString i}_${toString j}Actions" = { ActionsCount = 1; };
+          "Data_${toString i}_${toString j}Conditions" = { ConditionsCount = 0; };
+
+          "Data_${toString i}_${toString j}" = {
+            Comment = "";
+            Enabled = true;
+            Name = item.name;
+            Type = "SIMPLE_ACTION_DATA";
+          };
+
+          "Data_${toString i}_${toString j}Actions0" = {
+            CommandURL = item.command;
+            Type = "COMMAND_URL";
+          };
+
+          "Data_${toString i}_${toString j}Triggers" = {
+            Comment = "Simple_action";
+            TriggersCount = 1;
+          };
+
+          "Data_${toString i}_${toString j}Triggers0" = {
+            Key = item.key;
+            Type = "SHORTCUT";
+            Uuid = "{${item.uuid}}";
+          };
+        })
+        (builtins.attrValues cfg.hotkeys.${group}));
+    in
+    lib.recursiveUpdate
+      { Data.DataCount = builtins.length (builtins.attrNames cfg.hotkeys); }
+      groups;
+
   kWriteConfig = file: group: attrs:
     lib.concatStringsSep "\n" (lib.mapAttrsToList
       (key: value: ''
@@ -58,6 +133,37 @@ in
 {
   options.tilde.programs.plasma = {
     enable = lib.mkEnableOption "Configure KDE Plasma";
+
+    hotkeys = lib.mkOption {
+      default = { };
+      description = "Custom Key Shortcuts (khotkey)";
+      type = lib.types.attrsOf (lib.types.attrsOf
+        (lib.types.submodule ({ name, ... }: {
+          options = {
+            name = lib.mkOption {
+              type = lib.types.str;
+              default = name;
+              description = "Shortcut Name";
+            };
+
+            uuid = lib.mkOption {
+              type = lib.types.str;
+              example = "79f4521c-ccc2-4870-9b39-b52f1369cf21";
+              description = "A unique ID for this shortcut";
+            };
+
+            key = lib.mkOption {
+              type = lib.types.str;
+              description = "The key that triggers the action";
+            };
+
+            command = lib.mkOption {
+              type = lib.types.str;
+              description = "The command to execute";
+            };
+          };
+        })));
+    };
 
     bindings = lib.mkOption {
       default = { };
@@ -130,20 +236,27 @@ in
           "ExposeAll" = { keys = [ "Meta+." ]; display = "Toggle Present Windows (All desktops)"; };
           "ExposeClass" = { keys = none; display = "Toggle Present Windows (Window class)"; };
           "Kill Window".keys = [ "Meta+Shift+Q" ];
-          "MoveMouseToFocus".keys = [ "Meta+W" ];
+          "MoveMouseToFocus".keys = [ "Meta+Shift+W" ];
           "Show Desktop".keys = none;
-          "ShowDesktopGrid" = { keys = none; display = "Show Desktop Grid"; };
+          "ShowDesktopGrid" = { keys = [ "Meta+D" ]; display = "Show Desktop Grid"; };
+          "stop current activity" = { keys = none; display = "Stop Current Activity"; };
           "Suspend Compositing".keys = none;
           "Switch to Next Screen".keys = none;
           "Switch Window Down" = { keys = [ "Meta+J" ]; display = "Switch to Window Below"; };
           "Switch Window Left" = { keys = [ "Meta+H" ]; display = "Switch to Window to the Left"; };
           "Switch Window Right" = { keys = [ "Meta+L" ]; display = "Switch to Window to the Right"; };
           "Switch Window Up" = { keys = [ "Meta+K" ]; display = "Switch to Window Above"; };
+          "view_actual_size" = { keys = [ "Meta+=" ]; display = "Actual Size"; };
+          "view_zoom_in" = { keys = [ "Meta++" ]; display = "Zoom In"; };
+          "view_zoom_out" = { keys = [ "Meta+-" ]; display = "Zoom Out"; };
           "Walk Through Windows (Reverse)".keys = [ "Meta+~" ];
-          "Walk Through Windows of Current Application (Reverse)".keys = [ "Meta+Shift+Tab" ];
-          "Walk Through Windows of Current Application".keys = [ "Meta+Tab" ];
+          "Walk Through Windows Alternative (Reverse)".keys = [ "Meta+Shift+Tab" ];
+          "Walk Through Windows Alternative".keys = [ "Meta+Tab" ];
+          "Walk Through Windows of Current Application (Reverse)".keys = none;
+          "Walk Through Windows of Current Application".keys = none;
           "Walk Through Windows".keys = [ "Meta+`" ];
           "Window Close" = { keys = [ "Meta+Q" ]; display = "Close Window"; };
+          "Window Fullscreen" = { keys = [ "Meta+F11" ]; display = "Make Window Fullscreen"; };
           "Window Maximize" = { keys = [ "Meta+F" ]; display = "Maximize Window"; };
           "Window Minimize" = { keys = [ "Meta+M" ]; display = "Minimize Window"; };
           "Window One Desktop Down".keys = none;
@@ -155,11 +268,9 @@ in
           "Window Quick Tile Left" = { keys = [ "Meta+Shift+H" ]; display = "Quick Tile Window to the Left"; };
           "Window Quick Tile Right" = { keys = [ "Meta+Shift+L" ]; display = "Quick Tile Window to the Right"; };
           "Window Quick Tile Top" = { keys = [ "Meta+Shift+K" ]; display = "Quick Tile Window to the Top"; };
+          "Window Shade" = { keys = [ "Meta+S" ]; display = "Shade Window"; };
           "Window to Next Screen".keys = [ "Meta+>" ];
-          "Window to Previous Screen".keys = none;
-          "view_actual_size" = { keys = [ "Meta+=" ]; display = "Actual Size"; };
-          "view_zoom_in" = { keys = [ "Meta++" ]; display = "Zoom In"; };
-          "view_zoom_out" = { keys = [ "Meta+-" ]; display = "Zoom Out"; };
+          "Window to Previous Screen".keys = [ "Meta+<" ];
         } // (
           let
             shiftKeys = [ "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" ];
@@ -206,6 +317,34 @@ in
           (lib.range 1 10)));
       };
 
+    tilde.programs.plasma.hotkeys = {
+      "Gromit MPX" = {
+        "Gromit MPX Clear" = {
+          key = "F7";
+          command = "gromit-mpx --clear";
+          uuid = "79f4521c-ccc2-4870-9b39-b52f1369cf21";
+        };
+        "Gromit MPX Toggle" = {
+          key = "F8";
+          command = "gromit-mpx-toggle.sh";
+          uuid = "b6d4e1cc-3289-44cd-a022-87941ef85c31";
+        };
+      };
+
+      "General Purpose" = {
+        "Launch Emacs" = {
+          key = "Meta+E";
+          command = "e";
+          uuid = "e62c4fa6-d560-11ec-a0f7-5731faeb799a";
+        };
+        "Warp Mouse to Active Window" = {
+          key = "Meta+W";
+          command = "xdotool getwindowfocus mousemove --window %1 0 -20";
+          uuid = "585d1fb6-8722-4a31-871f-f8e1b49cd678";
+        };
+      };
+    };
+
     # Default settings.
     tilde.programs.plasma.settings = {
       kdeglobals = {
@@ -228,10 +367,11 @@ in
         Plugins.diminactiveEnabled = true;
         Plugins.sheetEnabled = true;
         TabBox.DesktopMode = 0;
-        TabBox.HighlightWindows = false;
-        TabBox.LayoutName = "present_windows";
+        TabBox.HighlightWindows = true;
+        TabBox.LayoutName = null;
+        TabBoxAlternative.ShowDesktopMode = 1;
         Windows.ElectricBorderMaximize = false;
-        Windows.ElectricBorders = 1;
+        Windows.ElectricBorders = null;
         Windows.FocusPolicy = "FocusFollowsMouse";
         Windows.Placement = "Smart";
         Windows.SeparateScreenFocus = true;
@@ -267,6 +407,7 @@ in
         "org.kde.plasma.emojier.desktop"._k_friendly_name = "Emoji Selector";
       };
     };
+
 
     home.packages = with pkgs; [
       crudini # For editing the GTK settings.ini file.
