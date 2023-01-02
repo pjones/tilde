c39e24e82044297a5d6ae15301b1424c9e9e1481
Peter Jones <pjones@devalot.com>
2023-01-02 10:02:52 -0700

    syncthing: Remove tray service
    I don't use it.

--- home/programs/syncthing.nix
+++ home/programs/syncthing.nix
@@ -16,12 +16,6 @@ in
         "--gui-address=0.0.0.0:8384" # Safe thanks to the firewall.
         "--no-default-folder"
       ];
-
-      tray = {
-        enable = lib.mkDefault config.tilde.xsession.enable;
-        command = "syncthingtray --wait";
-        package = pkgs.syncthingtray-minimal;
-      };
     };
   };
 }
