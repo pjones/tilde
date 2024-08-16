a11aa1d46d938dcd1a90c6d213eb98253d72e934
Peter Jones <pjones@devalot.com>
2024-08-16 12:48:05 -0700

    syncthing: Make it easier to override which interface is bound

--- home/programs/syncthing.nix
+++ home/programs/syncthing.nix
@@ -6,6 +6,18 @@ in
 {
   options.tilde.programs.syncthing = {
     enable = lib.mkEnableOption "Syncthing File Synchronization";
+
+    gui.ip = lib.mkOption {
+      type = lib.types.str;
+      default = "127.0.0.1";
+      description = "IP address to bind the GUI to";
+    };
+
+    gui.port = lib.mkOption {
+      type = lib.types.ints.positive;
+      default = 8384;
+      description = "The port the GUI will listen on";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -13,7 +25,7 @@ in
       enable = true;
 
       extraOptions = [
-        "--gui-address=0.0.0.0:8384" # Safe thanks to the firewall.
+        "--gui-address=${cfg.gui.ip}:${toString cfg.gui.port}"
         "--no-default-folder"
       ];
     };
