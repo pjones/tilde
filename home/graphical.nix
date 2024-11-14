137ca1fb950356af9f0cf7672dd696ecbade3634
Peter Jones <pjones@devalot.com>
2024-11-14 10:51:36 +0100

    theme: Use pinentry-qt because it supports Wayland

--- home/graphical.nix
+++ home/graphical.nix
@@ -43,7 +43,7 @@ in
       defaultCacheTtlSsh = 14400;
       maxCacheTtl = 7200;
       maxCacheTtlSsh = 21600;
-      pinentryPackage = pkgs.pinentry-gtk2;
+      pinentryPackage = pkgs.pinentry-qt;
     };
 
     xdg.desktopEntries = {
