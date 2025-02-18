141d53e643a3ca7d94cf86a97d2eaf6c6ec52302
Peter Jones <pjones@devalot.com>
2025-02-18 13:37:25 +0100

    flatpak: Enable and configure

--- nixos/workstation.nix
+++ nixos/workstation.nix
@@ -127,6 +127,21 @@ in
             autoPrune.enable = true;
           };
         };
+
+        # Flatpak:
+        #
+        # https://wiki.nixos.org/wiki/Flatpak
+        services.flatpak.enable = true;
+        systemd.services.flatpak-repo = {
+          after = [ "network-online.target" ];
+          wantedBy = [ "multi-user.target" ];
+          wants = [ "network-online.target" ];
+          path = [ pkgs.flatpak ];
+          script = ''
+            flatpak remote-add --if-not-exists \
+              flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+          '';
+        };
       })
       (lib.mkIf (cfg.enable && cfg.type == "laptop") {
         environment.systemPackages = with pkgs; [
