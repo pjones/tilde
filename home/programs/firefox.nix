140c1ff233d70b90228b0387ea164539550b29f2
Peter J. Jones <peter@jonesbunch.com>
2026-04-13 17:38:35 +0200

    firefox: Turning off context menus doesn't actually work
    They still show up, underneath the Firefox right-click menu!

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -38,7 +38,6 @@ let
     "browser.tabs.closeWindowWithLastTab" = false;
     "browser.tabs.inTitlebar" = 0;
     "browser.urlbar.trimURLs" = false;
-    "dom.event.contextmenu.enabled" = false;
     "dom.forms.autocomplete.formautofill" = false;
     "extensions.formautofill.addresses.enabled" = false;
     "extensions.formautofill.creditCards.enabled" = false;
